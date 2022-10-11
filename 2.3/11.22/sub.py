511d0fa89d094598a9ff3c40c18df8de6cd3b78c
Alexander-Domnenko <91257943+Alexander-Domnenko@users.noreply.github.com>
2022-10-11 17:33:34 +0800

    Add files via upload

---
+++ 2.3/11.22/sub.py
@@ -0,0 +1,15 @@
+class MyList(list):
+
+    def __init__(self, *args, **kwargs):
+        list.__init__(self, *args, **kwargs)
+
+    def __sub__(self, other):
+        a = self
+        r = []
+        for i in range(len(other)):
+            r.append(a[i]-other[i])
+        return r
+
+
+a = MyList([1, 2, 3])
+print([2, 3]-a)
